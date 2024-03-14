f7a2d727ffed9aad02ea4455465b88195c2985b1
Yosif Hamed <josif.hameed@gmail.com>
2024-03-15 00:52:31 +0200

    Add institution summary route

--- server/src/routers/index.ts
+++ server/src/routers/index.ts
@@ -2,11 +2,12 @@ import { Router } from 'express'
 import { getDb } from '../db'
 
 import { catchRoute } from './catchRoute'
+import { ComplaintType, Complaint } from '../interfaces'
 
 const router = Router()
 
 router.post('/', catchRoute(complaintPost))
-router.get('/by-institution/:id', catchRoute(complaintByInstitution))
+router.get('/by-institution/:institutionId', catchRoute(institutionSummary))
 router.get('/by-office/:id', catchRoute(complaintsByOffice))
 
 
@@ -38,9 +39,49 @@ async function complaintPost(req: any, res: any) {
 
 }
 
-async function complaintByInstitution(req:any,res:any){
-  // @TODO
-  // const complaints = await getDb().collection('complaints')
+async function institutionSummary(req:any,res:any){
+  const institutionId = parseInt(req.params.institutionId)
+  if(!institutionId) {
+    res.status(400).send({success:false, message:"institution id must be a number"})
+    return
+  }
+  const complaints = await getDb().collection('complaints').find({institutionId}).toArray()
+
+  let data:any = {
+    averages: {
+        "speed": 0,
+        "politeness": 0,
+        "precision": 0,
+        "tech": 0,
+        "accessability": 0,
+        "pricing": 0
+    },
+    historicalRating: {
+        "speed": [],
+        "politeness": [], 
+        "precision": [],
+        "tech": [],
+        "accessability": [],
+        "pricing": []
+    },
+    institutionId,
+    institututionName: "PLACEHOLDER"
+  }
+  complaints.forEach((c: Complaint) => {
+    data.averages[c.type as ComplaintType] += c.rating
+    data.historicalRating[c.type].push(c.rating)
+  })
+
+  Object.keys(data.averages).map((key)=>{
+    if (data.historicalRating.length) data.averages[key] /= data.historicalRating.length
+  })
+
+  // determines importance of complaintType based on number of complaints
+  data.mostRelevant =( Object.entries(data.historicalRating) as [string,number[]][]).reduce(
+    (a:[string,number[]],b:[string,number[]]):any => a[1].length>b[1].length ? a : b ,
+    ['',[]]
+  )[0]
+  res.status(200).send({success:true, data})
 }
 
 async function complaintsByOffice(req:any,res:any){
