67c7c053fe6b7dd735a39a8f69af90db4bb63aad
NikolayLazarov <nikilazt7@gmail.com>
2024-03-16 01:30:41 +0200

    Better Design Form, add mor stars to rating

--- front-end/src/components/SelectorOptions/Rating.jsx
+++ front-end/src/components/SelectorOptions/Rating.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 
 const Rating = () => {
   return (
-    <div className="rating rating-sm">
-    <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
-    <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" checked />
-    <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
-    <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
-    <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
-    </div>
+    <>
+      <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
+      <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400"  />
+      <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
+      <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
+      <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" checked/>
+      <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
+      <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400"  />
+      <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
+      <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
+      <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
+    
+    </>
   )
 }
 
