07150adaa5d28ba1b8931c1de56ec70d01e48469
NikolayLazarov <nikilazt7@gmail.com>
2024-03-15 20:51:23 +0200

    Avatar changes

--- front-end/src/components/Avatars/InstitutionAvatar.jsx
+++ front-end/src/components/Avatars/InstitutionAvatar.jsx
@@ -5,7 +5,7 @@ const InstitutionAvatar = ({imageInst}) => {
     <>
         <div className="avatar">
         <div className="w-14 h-14 rounded">
-            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" /* {imageList} */ />
+            <img src="././public/to-cut-svgrepo-com.svg" /* {imageList} */ />
         </div>
         </div>
     </>
