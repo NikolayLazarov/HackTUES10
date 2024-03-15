3edc05f6c66a2f164337fb532cbf677b583922d5
NikolayLazarov <nikilazt7@gmail.com>
2024-03-15 02:24:18 +0200

    Add ComplaintPage to routing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import {createBrowserRouter,RouterProvider} from "react-router-dom"
 import LandingPage from './pages/LandingPage';
 import ErrorPage from './pages/ErrorPage';
+import ComplaintPage from './pages/ComplaintPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <LandingPage />,
     errorElement: <ErrorPage />
-  },
+  },{
+    path: "/complaint",
+    element: <ComplaintPage />,
+    errorElement: <ErrorPage />
+  }
 ]);
 
 function App() {
