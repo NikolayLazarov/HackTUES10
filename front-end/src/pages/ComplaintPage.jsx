051e2b8967ad259a905290a682877ad8404ddb74
NikolayLazarov <nikilazt7@gmail.com>
2024-03-15 02:27:32 +0200

    Add remove 2 points of BreadcrumsStart. Add component to ComplaintPage

--- front-end/src/pages/ComplaintPage.jsx
+++ front-end/src/pages/ComplaintPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Nabvar'
+import BreadcrumsStart from '../components/BreadCrums/BreadcrumsStart'
 
 const ComplaintPage = () => {
   return (
     <>
         <Navbar />
 
-        <h2>Form for </h2>
-    
+        {/* <h2>Form for </h2> */}
+        <BreadcrumsStart />
     </>
   )
 }
