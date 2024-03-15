c8c820099d22802a5c260aa3b9c36d20bf540dec
NikolayLazarov <nikilazt7@gmail.com>
2024-03-16 00:45:36 +0200

    Restail Navbar

--- front-end/src/components/Navbar/Nabvar.jsx
+++ front-end/src/components/Navbar/Nabvar.jsx
@@ -3,32 +3,36 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 flex space-x-10 ">
           <div className="flex-1">
-            <a href="/" className="btn btn-ghost text-xl">daisyUI</a>
+            <a href="/" className="btn btn-ghost text-xl">Xranilka</a>
           </div>
 
           <div>
             <a href='/complaint' className='btn btn-error text-xl'>Feedback</a>
           </div>
 
-
-          <div className="flex-none">
+          <div className="avatar placeholder pr-5">
+          <div className="bg-success text-success-content rounded-full w-12">
+            <span className="text-xs">Me</span>
+          </div>
+        </div>
+          {/* <div className="flex-none">
             <ul className="menu menu-horizontal px-1">
               {/* <li><a>Link</a></li> */}
-              <li>
+              {/* <li>
                 <details>
                   <summary>
-                    Parent
+                    Profile
                   </summary>
-                  <ul className="p-2 bg-base-100 rounded-t-none">
+                  <ul className="p-2 bg-base-100 rounded-t-none"> */}
                     {/* <li><a>Link 1</a></li> */}
                     {/* <li><a>Link 2</a></li> */}
-                  </ul>
+                  {/* </ul>
                 </details>
               </li>
             </ul>
-          </div>
+          </div> */}
         </div>
     </>
   )
